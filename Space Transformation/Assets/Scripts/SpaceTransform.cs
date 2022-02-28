ebd75b30548c9981c65830ed0cfd36529b2a3c65
vicarAmelia <98558073+vicarAmelia@users.noreply.github.com>
2022-02-28 11:03:46 -0500

    Updated Space Transform script

--- Space Transformation/Assets/Scripts/SpaceTransform.cs
+++ Space Transformation/Assets/Scripts/SpaceTransform.cs
@@ -4,7 +4,8 @@ using UnityEngine;
 
 public class SpaceTransform : MonoBehaviour
 {
-    public Vector2 localSpacePoint;
+    public Vector2 worldSpacePoint;
+    public Transform localObjectTransform;
 
     private void OnDrawGizmos()
     {
@@ -14,13 +15,24 @@ public class SpaceTransform : MonoBehaviour
         Gizmos.DrawRay(transform.position, transform.up);
 
         Gizmos.color = Color.blue;
-        Vector2 worldSpacePoint = LocalToWorld(localSpacePoint);
+        
         Gizmos.DrawSphere(worldSpacePoint, 0.15f);
+
+        localObjectTransform.localPosition = WorldToLocal(worldSpacePoint);
     }
 
-    Vector2 LocalToWorld(Vector2 localPoint)
+    // Vector2 LocalToWorld(Vector2 localPoint)
+    // {
+    //     Vector2 worldOffset = (transform.right * localPoint.x) + (transform.up * localPoint.y);
+    //     return (Vector2)transform.position + worldOffset;
+    // }
+    
+    Vector2 WorldToLocal(Vector2 worldPoint)
     {
-        Vector2 worldOffset = (transform.right * localPoint.x) + (transform.up * localPoint.y);
-        return (Vector2)transform.position + worldOffset;
+        Vector2 relativePoint = worldPoint - (Vector2)transform.position;
+        float x = Vector2.Dot(relativePoint, transform.right);
+        float y = Vector2.Dot(relativePoint, transform.up);
+
+        return new Vector2(x,y);
     }
 }
